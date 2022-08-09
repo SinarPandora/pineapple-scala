7535b14f66d32be4f370be67f48c7f40cd51497a
Sinar Pandora <SinarPandora@outlook.com>
2022-08-09 22:49:01 +0900

    Add comments

--- src/main/scala/lexer/Lexer.scala
+++ src/main/scala/lexer/Lexer.scala
@@ -3,118 +3,206 @@ package lexer
 import lexer.Token.Token
 
 import java.lang.Character.isLetter
+import scala.annotation.tailrec
 import scala.util.matching.Regex
 
+/**
+ * 分词器
+ */
 class Lexer {
-  var sourceCode = ""
+  // 当前源码
+  var remainSourceCode: String = ""
+  // 下一个 Token
   private var nextToken: String = ""
+  // 当前行数
   private var lineNum: Int = 1
+  // 下一个 Token 行数
   private var nextTokenLineNum: Int = 0
+  // 下一个 Token 类型
   private var nextTokenType: Token = Token.EOF
+  // 提取名称的正则
+  private val namePattern: Regex = raw"^([_\d\w]+)".r
 
-  private val regexp: Regex = raw"^([_\d\w]+)".r
+  /**
+   * 获取行号
+   *
+   * @return 行号
+   */
+  def getLineNum: Int = lineNum
 
-  def getLineNum(): Int = lineNum
-
-  def skipSourceCode(i: Int) = {
-    sourceCode = sourceCode.substring(i)
+  /**
+   * 跳过一定位数的源码
+   *
+   * @param skip 跳过位数
+   */
+  def skipSourceCodeChars(skip: Int): Unit = {
+    remainSourceCode = remainSourceCode.substring(skip)
   }
 
+  /**
+   * 扫描名称
+   *
+   * @return 名称 -> Token
+   */
   def scanName(): (String, Option[Token]) = {
-    regexp.findFirstIn(sourceCode) match {
-      case Some(name) => skipSourceCode(name.length())
+    namePattern.findFirstIn(remainSourceCode) match {
+      case Some(name) =>
+        // 扫到了，提取，并让下次扫描跳过这个名称
+        skipSourceCodeChars(name.length())
         if (name == "print") {
+          // Print 是一个特殊的名称
           (name, Some(Token.PRINT))
-        }
-        else {
+        } else {
           (name, None)
         }
-      case _ => ("", None)
+      case _ => ("", None) // 没扫到名称
     }
   }
 
-  def nextSourceCodeIs(str: String): Boolean = sourceCode.startsWith(str)
+  /**
+   * 判断下一部分源码是否以目标字符串开头
+   *
+   * @param startWith 目标字符串
+   * @return 判断结果
+   */
+  def nextSourceCodeIs(startWith: String): Boolean = remainSourceCode.startsWith(startWith)
 
-  def isIgnored(): Boolean = {
-    def isIgnoredAux(b: Boolean, i: Int): Boolean = {
-      if (i >= sourceCode.length) {
-        skipSourceCode(i)
-        b
+  /**
+   * 判断下面是否是有意义的源码
+   *
+   * @return 判断结果
+   */
+  def nextIsNotSource: Boolean = {
+    /**
+     * 源码扫描
+     *
+     * @param notFound 未找到源码（继续扫描）
+     * @param skip     跳过（位数）
+     * @return 判断结果
+     */
+    @tailrec def innerScan(notFound: Boolean, skip: Int): Boolean = {
+      if (skip >= remainSourceCode.length) {
+        // 如果跳过了所有的源码
+        // 移动扫描标志位
+        skipSourceCodeChars(skip)
+        // 返回 true
+        true
       } else {
+        // 跳过空白字符并继续扫描
         if (nextSourceCodeIs("\r\n") || nextSourceCodeIs("\n\r")) {
-          lineNum += 1
-          isIgnoredAux(true, i + 2)
+          lineNum += 1 // 遇到 windows 换行时直接前进一行
+          innerScan(notFound = true, skip + 2)
         } else {
-          sourceCode(i) match {
+          remainSourceCode(skip) match {
             case '\n' | '\r' =>
-              lineNum += 1
-              isIgnoredAux(true, i + 1)
+              lineNum += 1 // 遇到 *nix 换行时直接前进一行
+              innerScan(notFound = true, skip + 1)
             case '\t' | '\f' | ' ' =>
-              isIgnoredAux(true, i + 1)
-            case _ => skipSourceCode(i)
-              b
+              innerScan(notFound = true, skip + 1) // 遇到空白字符时直接前进一行
+            case _ =>
+              // 发现有意义字符时
+              // 跳过无意义字符
+              skipSourceCodeChars(skip)
+              // 返回 false
+              false
           }
         }
       }
     }
 
-    isIgnoredAux(false, 0)
+    innerScan(notFound = false, 0)
   }
 
+  /**
+   * 识别 Token
+   *
+   * @return 行数，类型，值
+   */
   def MatchToken(): Option[(Int, Token.Value, String)] = {
-    if (sourceCode.length == 0) {
+    // 如果剩余源码为空，EOF
+    if (remainSourceCode.isEmpty) {
       Some(lineNum, Token.EOF, "EOF")
     } else {
-      sourceCode(0) match {
-        case '$' => skipSourceCode(1)
+      // 取 1 个字符
+      remainSourceCode(0) match {
+        case '$' =>
+          skipSourceCodeChars(1)
           Some(lineNum, Token.VAR_PREFIX, "$")
-        case '(' => skipSourceCode(1)
+        case '(' =>
+          skipSourceCodeChars(1)
           Some(lineNum, Token.LEFT_PAREN, "(")
-        case ')' => skipSourceCode(1)
+        case ')' =>
+          skipSourceCodeChars(1)
           Some(lineNum, Token.RIGHT_PAREN, ")")
-        case '=' => skipSourceCode(1)
+        case '=' =>
+          skipSourceCodeChars(1)
           Some(lineNum, Token.EQUAL, "=")
-        case '\"' =>
+        case '"' => // 如果是字符串，优先判断是否为空字符串
           if (nextSourceCodeIs("\"\"")) {
-            skipSourceCode(2)
+            // 如果是空字符串，直接跳过两个字符
+            skipSourceCodeChars(2)
             Some(lineNum, Token.DUOQUOTE, "\"\"")
           } else {
-            skipSourceCode(1)
+            // 如果是一个引号，正常处理
+            skipSourceCodeChars(1)
             Some(lineNum, Token.QUOTE, "\"")
           }
         case s@_ => if (s == '_' || isLetter(s)) {
+          // 扫描名称
           scanName() match {
             case (token, Some(tokenType)) => Some(lineNum, tokenType, token)
             case (token, None) => Some(lineNum, Token.NAME, token)
           }
-        } else if (isIgnored()) {
-          Some(lineNum, Token.IGNORED, s.toString())
+        } else if (nextIsNotSource) {
+          // 如果没有源码，标记为 Ignored
+          Some(lineNum, Token.IGNORED, s.toString)
         } else {
-          printf("MatchToken(): unexpected symbol near '%c' at line %n.", s.toString(), lineNum)
+          printf("MatchToken(): unexpected symbol near '%c' at line %n.", s, lineNum)
           None
         }
       }
     }
   }
 
-  def scanBeforeToken(token: String): Option[String] = {
-    def scanBeforeTokenAux(token: String, i: Int): Option[String] = {
-      if (i >= sourceCode.length) {
-        printf("scanBeforeToken(): expect a(n) %s.", token)
+  /**
+   * 扫描，直到 Token
+   *
+   * @param target 目标
+   * @return 扫描结果
+   */
+  def scanUntilToken(target: String): Option[String] = {
+
+    /**
+     * 内部扫描
+     *
+     * @param length 扫描到的长度
+     * @return 扫描结果
+     */
+    @tailrec def innerScan(length: Int): Option[String] = {
+      if (length >= remainSourceCode.length) {
+        // 如果已经到代码的最后还没找到目标 Token
+        printf("scanBeforeToken(): expect a(n) %s.", target)
         None
-      } else if (sourceCode.startsWith(token, i)) {
-        val str = sourceCode.substring(0, i)
-        skipSourceCode(i)
+      } else if (remainSourceCode.startsWith(target, length)) {
+        // 如果已经找到了目标 Token
+        val str = remainSourceCode.substring(0, length)
+        skipSourceCodeChars(length)
         Some(str)
       } else {
-        scanBeforeTokenAux(token, i + 1)
+        // 没找到时持续 + 1
+        innerScan(length + 1)
       }
     }
 
-    scanBeforeTokenAux(token, 0)
+    innerScan(0)
   }
 
 
+  /**
+   * 获取下一个 Token
+   *
+   * @return 下一个 Token
+   */
   def GetNextToken(): Option[(Int, Token, String)] = {
     if (nextTokenLineNum > 0) {
       lineNum = nextTokenLineNum
@@ -125,18 +213,30 @@ class Lexer {
     }
   }
 
-  def NextTokenIs(token: Token): Option[(Int, String)] = {
+  /**
+   * 寻找下一个指定的 Token
+   *
+   * @param token 目标 Token
+   * @return 如果找到了，返回结果
+   */
+  def FindAndMoveToNextTokenWithType(token: Token): Option[(Int, String)] = {
     GetNextToken() match {
-      case Some((nowLineNum, nowTokenType, nowToken)) if (nowTokenType == token) => Some(nowLineNum, nowToken)
+      case Some((nowLineNum, nowTokenType, nowToken)) if nowTokenType == token => Some(nowLineNum, nowToken)
       case Some((_, nowTokenType, nowToken)) =>
-        printf("NextTokenIs(): syntax error near '%s', expected token: {%s} but got {%s}.", nowToken, token, nowTokenType)
+        printf("findNextTokenWithType(): syntax error near '%s', expected token: {%s} but got {%s}.", nowToken, token, nowTokenType)
         None
       case None => None
     }
   }
 
+  /**
+   * 寻找下一个任意 Token
+   *
+   * @return 任意 Token
+   */
   def LookAhead(): Option[Token] = {
     if (nextTokenLineNum > 0) {
+      // 如果已经寻找过下一个 Token，直接返回缓存
       Some(nextTokenType)
     } else {
       val nowLineNum = lineNum
@@ -152,10 +252,15 @@ class Lexer {
     }
   }
 
+  /**
+   * 仅寻找下一个目标 Token，并忽略之前的全部内容
+   *
+   * @param expectedTokenType 目标 Token 类型
+   */
   def LookAheadAndSkip(expectedTokenType: Token): Unit = {
     val nowLineNum = lineNum
     GetNextToken() match {
-      case Some((lineNum, tokenType, token)) if (tokenType != expectedTokenType) =>
+      case Some((lineNum, tokenType, token)) if tokenType != expectedTokenType =>
         this.lineNum = nowLineNum
         this.nextTokenType = tokenType
         this.nextToken = token
--- src/main/scala/parser/Parser.scala
+++ src/main/scala/parser/Parser.scala
@@ -1,49 +1,84 @@
 package parser
 
-import asts._
+import asts.*
 import lexer.{Lexer, Token}
 
+import scala.annotation.tailrec
 import scala.language.postfixOps
 
+/**
+ * 解析器
+ */
 object Parser {
-  var lexer = new Lexer()
+  // 分词器
+  var lexer: Lexer = new Lexer()
 
-  def parseName(): Option[String] = {
-    lexer.NextTokenIs(Token.NAME) match {
+  /**
+   * 解析名称
+   *
+   * @return 解析结果
+   */
+  def parseName(): Option[String] =
+    lexer.FindAndMoveToNextTokenWithType(Token.NAME) match {
       case Some((_, name)) => Some(name)
-      case _ =>
-        None
+      case _ => None
     }
-  }
 
+  /**
+   * 解析字符串
+   *
+   * @return 解析结果
+   */
   def parseString(): Option[String] = {
     lexer.LookAhead() match {
       case Some(Token.DUOQUOTE) =>
-        lexer.NextTokenIs(Token.DUOQUOTE)
+        // 查找并移动到双引号后面
+        lexer.FindAndMoveToNextTokenWithType(Token.DUOQUOTE)
+        // 令标志位跳过所有空白字符
         lexer.LookAheadAndSkip(Token.IGNORED)
+        // 返回解析结果
         Some("")
       case Some(Token.QUOTE) =>
-        lexer.NextTokenIs(Token.QUOTE)
-        lexer.scanBeforeToken("\"") match {
+        // 查找并移动到第一个引号后边
+        lexer.FindAndMoveToNextTokenWithType(Token.QUOTE)
+        // 查找，直到下一个引号
+        lexer.scanUntilToken("\"") match {
           case Some(str) =>
-            lexer.NextTokenIs(Token.QUOTE)
+            // 将标志位移动到后一个引号后边
+            lexer.FindAndMoveToNextTokenWithType(Token.QUOTE)
+            // 跳过所有空白字符
             lexer.LookAheadAndSkip(Token.IGNORED)
+            // 返回解析结果
             Some(str)
-          case None => None
+          case None =>
+            None
         }
-      case _ => printf("parseString(): not a string.")
+      case _ =>
+        println("parseString(): not a string.")
         None
     }
   }
 
+  /**
+   * 解析变量
+   *
+   * @return 解析结果
+   */
   def parseVariable(): Option[Variable] = {
-    val lineNum = lexer.getLineNum()
-    lexer.NextTokenIs(Token.VAR_PREFIX) match {
+    // 获取行号，行号是用来提示错误信息的
+    val lineNum = lexer.getLineNum
+    // 解析变量符号
+    lexer.FindAndMoveToNextTokenWithType(Token.VAR_PREFIX) match {
       case Some(_) =>
+        // 变量符号后应该接变量名
         parseName() match {
-          case Some(name) => lexer.LookAheadAndSkip(Token.IGNORED)
+          case Some(name) =>
+            // 跳过变量后面的空白内容
+            lexer.LookAheadAndSkip(Token.IGNORED)
+            // 返回解析结果
             Some(Variable(lineNum, name))
           case _ =>
+            // 如果只有变量符号，输出提示
             printf("parseVariable(): expect a variable name at line %n", lineNum)
             None
         }
@@ -53,14 +88,28 @@ object Parser {
     }
   }
 
+  /**
+   * 解析赋值
+   *
+   * @return 解析结果
+   */
   def parseAssignment(): Option[Assignment] = {
-    val lineNum = lexer.getLineNum()
+    val lineNum = lexer.getLineNum
     parseVariable() match {
-      case Some(variable) => lexer.LookAheadAndSkip(Token.IGNORED)
-        lexer.NextTokenIs(Token.EQUAL) match {
-          case Some(_) => lexer.LookAheadAndSkip(Token.IGNORED)
+      case Some(variable) =>
+        // 找到变量后，跳过空白字符
+        lexer.LookAheadAndSkip(Token.IGNORED)
+        // 寻找赋值符号
+        lexer.FindAndMoveToNextTokenWithType(Token.EQUAL) match {
+          case Some(_) =>
+            // 找到赋值符号后，跳过空白字符
+            lexer.LookAheadAndSkip(Token.IGNORED)
+            // 查找可赋值的值（此处只支持字符串）
             parseString() match {
-              case Some(str) => lexer.LookAheadAndSkip(Token.IGNORED)
+              case Some(str) =>
+                // 跳过空白部分
+                lexer.LookAheadAndSkip(Token.IGNORED)
+                // 返回赋值 AST
                 Some(Assignment(lineNum, variable, str))
               case _ => None
             }
@@ -70,16 +119,32 @@ object Parser {
     }
   }
 
+  /**
+   * 解析输出语句
+   *
+   * @return 解析结果
+   */
   def parsePrint(): Option[Print] = {
-    val lineNum = lexer.getLineNum()
-    lexer.NextTokenIs(Token.PRINT) match {
+    val lineNum = lexer.getLineNum
+    // 寻找输出符号
+    lexer.FindAndMoveToNextTokenWithType(Token.PRINT) match {
       case Some(_) =>
-        lexer.NextTokenIs(Token.LEFT_PAREN) match {
-          case Some(_) => lexer.LookAheadAndSkip(Token.IGNORED)
+        // 寻找左括号
+        lexer.FindAndMoveToNextTokenWithType(Token.LEFT_PAREN) match {
+          case Some(_) =>
+            // 跳过空白部分
+            lexer.LookAheadAndSkip(Token.IGNORED)
+            // 寻找变量
             parseVariable() match {
-              case Some(variable) => lexer.LookAheadAndSkip(Token.IGNORED)
-                lexer.NextTokenIs(Token.RIGHT_PAREN) match {
-                  case Some(_) => lexer.LookAheadAndSkip(Token.IGNORED)
+              case Some(variable) =>
+                // 跳过空白部分
+                lexer.LookAheadAndSkip(Token.IGNORED)
+                // 寻找右括号
+                lexer.FindAndMoveToNextTokenWithType(Token.RIGHT_PAREN) match {
+                  case Some(_) =>
+                    // 跳过空白部分
+                    lexer.LookAheadAndSkip(Token.IGNORED)
+                    // 返回输出结果
                     Some(Print(lineNum, variable))
                   case None => None
                 }
@@ -91,42 +156,75 @@ object Parser {
     }
   }
 
+  /**
+   * 解析语句（组装方法）
+   *
+   * @return 解析结果
+   */
   def parseStatement(): Option[Statement] = {
+    // 根据下一个 Token 判断解析目标（这句话像……）
     lexer.LookAhead() match {
       case Some(Token.PRINT) => parsePrint()
       case Some(Token.VAR_PREFIX) => parseAssignment()
-      case _ => printf("parseStatement(): unknown Ast.Statement.")
-        None
+      case _ => printf("parseStatement(): unknown Ast.Statement."); None
     }
   }
 
+  /**
+   * 循环解析语句
+   *
+   * @return 解析结果
+   */
   def parseStatements(): Option[List[Statement]] = {
-    def parseStatementsAux(statements: List[Statement]): Option[List[Statement]] = {
+    /**
+     * 内部方法，循环解析语句
+     *
+     * @param statements 已经扫到的语句
+     * @return 解析结果
+     */
+    @tailrec def innerScan(statements: List[Statement]): Option[List[Statement]] = {
       lexer.LookAhead() match {
+        // 如果是 EOF 就结束扫描
         case Some(Token.EOF) => Some(statements)
+        // 如果存在内容就尝试解析语句
         case Some(_) => parseStatement() match {
-          case Some(stmt) => parseStatementsAux(statements ::: List(stmt))
+          case Some(stmt) =>
+            // 如果解析到了语句，添加到列表并继续解析
+            innerScan(statements ::: List(stmt))
           case None => None
         }
         case None => None
       }
     }
 
-    parseStatementsAux(Nil)
+    innerScan(Nil)
   }
 
+  /**
+   * 解析源码
+   *
+   * @return 解析结果
+   */
   def parseSourceCode(): Option[SourceCode] = {
-    val lineNum = lexer.getLineNum()
+    val lineNum = lexer.getLineNum
     parseStatements() match {
       case Some(stmts) => Some(SourceCode(lineNum, stmts))
       case None => None
     }
   }
 
+  /**
+   * 解析代码
+   *
+   * @param code 源码
+   * @return 解析结果
+   */
   def parse(code: String): Option[SourceCode] = {
-    lexer.sourceCode = code
+    lexer.remainSourceCode = code
     parseSourceCode() match {
-      case Some(sc) => lexer.NextTokenIs(Token.EOF)
+      case Some(sc) =>
+        // 期望下一个 token 是 EOF，如果不是就报错
+        lexer.FindAndMoveToNextTokenWithType(Token.EOF)
         Some(sc)
       case None => None
     }
